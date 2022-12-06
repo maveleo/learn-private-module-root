f56a15c559a3efdbac15c5496384fb101fcc7675
maveleo <leonardo.marini@gmail.com>
2022-12-06 20:07:46 +0000

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -6,3 +6,15 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = var.region
+}
+
+module "s3-webapp" {
+  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  name   = var.name
+  region = var.region
+  prefix = var.prefix
+  version = "1.0.0"
+}
